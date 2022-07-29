14e78ba8786ffbe19b2867852429cf8569717e37
Bas van Stein <bas9112@gmail.com>
2022-07-29 21:44:17 +0200

    Added requirements file and save and load model

--- doe2vec.py
+++ doe2vec.py
@@ -1,57 +1,67 @@
-from modulesRandFunc import generate_tree as genTree
-from modulesRandFunc import generate_tree2exp as genTree2exp
-from modulesRandFunc import generate_exp2fun as genExp2fun
-from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib import cm
 import tensorflow as tf
+from matplotlib import cm
+from mpl_toolkits import mplot3d
+from scipy.stats import qmc
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, losses
 from tensorflow.keras.models import Model
-from scipy.stats import qmc
-import numpy as np
 
-d = 2
-m = 8 #power of 2 for sample size
+from modulesRandFunc import generate_exp2fun as genExp2fun
+from modulesRandFunc import generate_tree as genTree
+from modulesRandFunc import generate_tree2exp as genTree2exp
+
+d = 5
+m = 8  # power of 2 for sample size
 seed = 0
 
+
 class Autoencoder(Model):
-  def __init__(self, latent_dim, sample_size):
-    super(Autoencoder, self).__init__()
-    self.latent_dim = latent_dim   
-    self.encoder = tf.keras.Sequential([
-      layers.Dense(sample_size / 2, activation='relu'),
-      layers.Dense(sample_size / 4, activation='relu'),
-      layers.Dense(latent_dim, activation='relu'),
-    ])
-    self.decoder = tf.keras.Sequential([
-      layers.Dense(sample_size / 4, activation='relu'),
-      layers.Dense(sample_size / 2, activation='relu'),
-      layers.Dense(sample_size, activation='sigmoid')
-    ])
+    def __init__(self, latent_dim, sample_size):
+        super(Autoencoder, self).__init__()
+        self.latent_dim = latent_dim
+        self.encoder = tf.keras.Sequential(
+            [
+                layers.Dense(sample_size / 2, activation="relu"),
+                layers.Dense(sample_size / 4, activation="relu"),
+                layers.Dense(latent_dim, activation="relu"),
+            ]
+        )
+        self.decoder = tf.keras.Sequential(
+            [
+                layers.Dense(sample_size / 4, activation="relu"),
+                layers.Dense(sample_size / 2, activation="relu"),
+                layers.Dense(sample_size, activation="sigmoid"),
+            ]
+        )
 
-  def call(self, x):
-    encoded = self.encoder(x)
-    decoded = self.decoder(encoded)
-    return decoded
+    def call(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
 
-class Doe2Vec():
+
+class Doe2Vec:
     def __init__(self, dim, m, n=1000, latent_dim=32, seed=0):
         self.dim = dim
         self.m = m
         self.n = n
         self.latent_dim = latent_dim
         self.seed = seed
-        #generate the DOE using Sobol
-        self.sampler = qmc.Sobol(d=self.dim, scramble=False, seed = self.seed)
+        # generate the DOE using Sobol
+        self.sampler = qmc.Sobol(d=self.dim, scramble=False, seed=self.seed)
         self.sample = self.sampler.random_base2(m=self.m)
 
+    def load(self, dir="models"):
+        self.autoencoder = tf.keras.models.load_model(f"{dir}/doe2vec_{self.dim}-{self.m}-{self.latent_dim}-{self.seed}")
+        print("Loaded pre-existng model")
+        self.summary()
 
     def generateData(self):
-        array_x = self.sample #it is required to be named array_x for the eval
+        array_x = self.sample  # it is required to be named array_x for the eval
         self.Y = []
         tries = 0
         while len(self.Y) < self.n:
@@ -59,15 +69,25 @@ class Doe2Vec():
             # create an artificial function
             tree = genTree.generate_tree(6, 12)
             exp = genTree2exp.generate_tree2exp(tree)
-            fun = genExp2fun.generate_exp2fun(exp, len(self.sample), self.sample.shape[1])
+            fun = genExp2fun.generate_exp2fun(
+                exp, len(self.sample), self.sample.shape[1]
+            )
             try:
                 array_y = eval(fun)
-                if (np.isnan(array_y).any() or np.isinf(array_y).any() or np.any(abs(array_y)<1e-8) or np.any(abs(array_y)>1e8) 
-                    or np.var(array_y)<1.0 or array_y.ndim!=1):
+                if (
+                    np.isnan(array_y).any()
+                    or np.isinf(array_y).any()
+                    or np.any(abs(array_y) < 1e-8)
+                    or np.any(abs(array_y) > 1e8)
+                    or np.var(array_y) < 1.0
+                    or array_y.ndim != 1
+                ):
                     continue
-                #normalize the train data (this should be done per row (not per column!))
+                # normalize the train data (this should be done per row (not per column!))
                 array_y = array_y.flatten()
-                array_y = (array_y - np.min(array_y)) / (np.max(array_y) - np.min(array_y))
+                array_y = (array_y - np.min(array_y)) / (
+                    np.max(array_y) - np.min(array_y)
+                )
                 self.Y.append(array_y)
             except:
                 continue
@@ -76,24 +96,47 @@ class Doe2Vec():
         self.test_data = tf.cast(self.Y[-50:], tf.float32)
         return self.Y
 
-    def compileAutoEncoder(self):
+    def compile(self):
         self.autoencoder = Autoencoder(self.latent_dim, self.Y.shape[1])
-        self.autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
+        self.autoencoder.compile(optimizer="adam", loss=losses.MeanSquaredError())
 
-    def train(self, epochs=50):
-        self.autoencoder.fit(self.train_data, self.train_data,
-                epochs=epochs,
-                shuffle=True,
-                validation_data=(self.test_data, self.test_data))
+    def fit(self, epochs=50):
+        self.autoencoder.fit(
+            self.train_data,
+            self.train_data,
+            epochs=epochs,
+            shuffle=True,
+            validation_data=(self.test_data, self.test_data),
+        )
+
+    def save(self, dir="models"):
+        #Save the mdoel
+        self.autoencoder.save(f"{dir}/doe2vec_{self.dim}-{self.m}-{self.latent_dim}-{self.seed}")
+
+    def encode(self, X, return_error=False):
+        encoded_doe = self.autoencoder.encoder([X]).numpy()
+        if return_error:
+            decoded_doe = self.autoencoder.decoder([encoded_doe]).numpy()
+            # todo: return reconstruction error
+        return encoded_doe
+
+    def summary(self):
+        self.autoencoder.summary()
 
     def visualizeTestData(self, n=10):
         encoded_does = self.autoencoder.encoder(self.test_data).numpy()
         decoded_does = self.autoencoder.decoder(encoded_does).numpy()
-        fig = plt.figure(figsize=(n*3, 8))
+        fig = plt.figure(figsize=(n * 3, 8))
         for i in range(n):
             # display original
-            ax = fig.add_subplot(2, n, i+1, projection='3d')
-            ax.plot_trisurf(self.sample[:,0], self.sample[:,1], self.test_data[i],cmap=cm.jet, antialiased = True)
+            ax = fig.add_subplot(2, n, i + 1, projection="3d")
+            ax.plot_trisurf(
+                self.sample[:, 0],
+                self.sample[:, 1],
+                self.test_data[i],
+                cmap=cm.jet,
+                antialiased=True,
+            )
             ax.xaxis.set_ticklabels([])
             ax.yaxis.set_ticklabels([])
             ax.zaxis.set_ticklabels([])
@@ -108,8 +151,14 @@ class Doe2Vec():
             plt.gray()
 
             # display reconstruction
-            ax = fig.add_subplot(2, n, i+1+n, projection='3d')
-            ax.plot_trisurf(self.sample[:,0], self.sample[:,1],decoded_does[i],cmap=cm.jet,antialiased = True)
+            ax = fig.add_subplot(2, n, i + 1 + n, projection="3d")
+            ax.plot_trisurf(
+                self.sample[:, 0],
+                self.sample[:, 1],
+                decoded_does[i],
+                cmap=cm.jet,
+                antialiased=True,
+            )
             ax.xaxis.set_ticklabels([])
             ax.yaxis.set_ticklabels([])
             ax.zaxis.set_ticklabels([])
@@ -124,6 +173,7 @@ class Doe2Vec():
             plt.gray()
         plt.tight_layout()
         plt.show()
+
 
 obj = Doe2Vec(d, m, n=10000)
 y = np.array(obj.generateData())
